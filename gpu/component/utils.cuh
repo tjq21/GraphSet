a27a7e20f9a78360c1f1ba4e2bbecab11f2de645
滕家齐 <tjq21@mails.tsinghua.edu.cn>
2023-09-08 22:58:12 +0800

    Add CUDA error code for debugging

--- gpu/component/utils.cuh
+++ gpu/component/utils.cuh
@@ -6,7 +6,7 @@
     { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true) {
     if (code != cudaSuccess) {
-        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        fprintf(stderr, "GPUassert %d: %s %s %d\n", code, cudaGetErrorString(code), file, line);
         if (abort)
             exit(code);
     }
